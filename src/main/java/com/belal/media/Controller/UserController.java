d843db25f4d7d062f2b2400906838822d05ed0a7
belaletech <belalahmadswnbhr@gmail.com>
2024-06-04 02:12:20 +0530

    update

--- src/main/java/com/belal/media/Controller/UserController.java
+++ src/main/java/com/belal/media/Controller/UserController.java
@@ -2,8 +2,7 @@ package com.belal.media.Controller;
 
 
 import com.belal.media.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +12,37 @@ public class UserController {
 
 
     @GetMapping("/users")
-    public List<User> getUsers()
+    public List<User> getUser()
     {
         List<User> users=new ArrayList<>();
-        User user1=new User("code","Belal","codewithbelal@gmail.com","123");
-        User user2=new User("ayan","Raza","codewithraza@gmail.com","121");
+        User user1=new User(1,"code","Belal","codewithbelal@gmail.com","123");
+        User user2=new User(2,"ayan","Raza","codewithraza@gmail.com","121");
         users.add(user1);
         users.add(user2);
         return users;
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getUserById(@PathVariable("userId")Integer id)
+    {
+        User user1=new User(1,"code","Belal","codewithbelal@gmail.com","123");
+        user1.setId(id);
+        return user1;
+    }
+
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user)
+    {
+        User newUser=new User();
+        newUser.setId(user.getId());
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+
+
+        return newUser;
     }
 
 }
--- src/main/java/com/belal/media/models/User.java
+++ src/main/java/com/belal/media/models/User.java
@@ -3,7 +3,7 @@ package com.belal.media.models;
 
 
 public class User {
-
+    private int id;
     private String firstName;
     private String lastName;
     private String email;
@@ -14,12 +14,21 @@ public class User {
 //        Todo Auto Generated constructor stub
     }
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(int id, String firstName, String lastName, String email, String password) {
         super();
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirstName() {
